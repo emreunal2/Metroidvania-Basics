4248cf2d7de3a10a7dd013179adaade264d00223
flaxyars <74737840+emreunal2@users.noreply.github.com>
2022-09-18 01:27:13 +0300

    Skills 1/2

--- MetroidBasics/Assets/Scripts/Player/PlayerMovement.cs
+++ MetroidBasics/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,14 +7,23 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] BulletController bullet;
     [SerializeField] Transform shotPoint;
-    [SerializeField] float moveSpeed;
-    [SerializeField] float jumpForce;
+    
+    [SerializeField] float moveSpeed, jumpForce;
 
     [SerializeField] LayerMask ground;
     private bool isOnGround;
     [SerializeField] Transform groundPoint;
     private int jumpCount;
+
     [SerializeField] Animator anim;
+
+    [SerializeField] float dashSpeed, dashTime;
+    private float dashCounter;
+    [SerializeField] float dashCooldown;
+    private float dashCooldownCounter;
+
+    [SerializeField] GameObject standing, ball;
+    [SerializeField] float ballCooldown, ballCooldownCounter;
 
     void Start()
     {
@@ -23,6 +32,75 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (dashCooldownCounter > 0)
+        {
+            dashCooldownCounter -= Time.deltaTime;
+        }
+        else
+        {
+            PlayerDash();
+        }
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
+        }
+        else
+        {
+            PlayerMove();
+        }
+
+        
+        PlayerJump();
+        PlayerFire();
+        if (ballCooldownCounter > 0)
+        {
+            ballCooldownCounter -= Time.deltaTime;
+        }
+        if (!ball.activeSelf)
+        {
+            if (Input.GetAxisRaw("Vertical") < -.9f)
+            {
+                if (ballCooldownCounter <= 0)
+                {
+                    ball.SetActive(true);
+                    standing.SetActive(false);
+                    ballCooldownCounter = ballCooldown;
+                }
+            }
+        }
+        else
+        {
+            if (!standing.activeSelf)
+            {
+                if (Input.GetAxisRaw("Vertical") > .9f)
+                {
+                    if (ballCooldownCounter <= 0)
+                    {
+                        ball.SetActive(false);
+                        standing.SetActive(true);
+                        ballCooldownCounter = ballCooldown;
+                    }
+                }
+            }
+        }
+
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+         
+    }
+
+    private void PlayerDash()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            dashCounter = dashTime;
+            dashCooldownCounter = dashCooldown;
+        }
+    }
+
+    private void PlayerMove()
     {
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, ground);
@@ -35,23 +113,32 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = Vector3.one;
         }
+    }
 
-        if(isOnGround)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {               
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
-        }
-
-        if (Input.GetButtonDown("Fire1"))
+    private void PlayerFire()
+    {
+        if (Input.GetButtonDown("Fire1") && standing.activeSelf)
         {
             anim.SetTrigger("isShooting");
             Instantiate(bullet, shotPoint.position, shotPoint.rotation).MoveDir = new Vector2(transform.localScale.x, 0f);
-           
         }
-        anim.SetBool("isOnGround", isOnGround);
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-         
+    }
+
+    private void PlayerJump()
+    {
+        if (isOnGround)
+        {
+            jumpCount = 0;
+        }
+        if (Input.GetButtonDown("Jump") && jumpCount < 1)
+        {
+            jumpCount++;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            if (jumpCount == 1)
+            {
+                anim.SetTrigger("isDoubleJump");
+            }
+        }
     }
 }
